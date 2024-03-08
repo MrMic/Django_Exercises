73d892bfbe6b0368469e069eed191b82f7ead450
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-08 21:37:52 +0100

    (feat) UI/UX: Login Form & User Authentication

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "users"
@@ -8,5 +9,10 @@ urlpatterns = [
         "register/",
         views.register_view,
         name="register",
+    ),
+    path(
+        "login/",
+        views.login_view,
+        name="login",
     ),
 ]
